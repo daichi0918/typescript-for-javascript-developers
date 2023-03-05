3af4a5900430a43eb85230b597257411d0d6f9e4
daichi0918 <kayasugadaichi@icloud.com>
2023-03-05 22:29:44 +0900

    create:enum.ts

---
+++ src/enum.ts
@@ -0,0 +1,41 @@
+export {};
+
+enum Months {
+  January = 1,
+  Feburuary,
+  March,
+  April,
+  May,
+  Jun,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+console.log(Months.January);
+console.log(Months.Feburuary);
+console.log(Months.December);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BLUE = '#0000FF',
+  // YELLOW = '#FFF000',
+  BLACK = '#000000'
+}
+
+let green = COLORS.GREEN
+console.log({ green });
+
+enum COLORS {
+  YELLOW = '#FFF000' 
+}
+
+COLORS.YELLOW;
+
+
+
